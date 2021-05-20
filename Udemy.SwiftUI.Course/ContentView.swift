7a82234da1cb1c28190207ba6a564e2f03853dd3
gene.dimitrow <e.dimitrow@gmail.com>
2021-05-20 21:13:02 +0300

    -course initial

---
+++ Udemy.SwiftUI.Course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udemy.SwiftUI.Course
+//
+//  Created by Gene Dimitrow on 20.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udemy.SwiftUI.Course/Udemy_SwiftUI_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_SwiftUI_CourseApp.swift
+//  Udemy.SwiftUI.Course
+//
+//  Created by Gene Dimitrow on 20.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_SwiftUI_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
