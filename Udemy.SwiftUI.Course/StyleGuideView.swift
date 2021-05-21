29def2b9c95a3bd1f161943c08c2d431f9ffa519
gene.dimitrow <e.dimitrow@gmail.com>
2021-05-21 11:29:51 +0300

    -wip: gradients

--- Udemy.SwiftUI.Course/StyleGuideView.swift
+++ Udemy.SwiftUI.Course/StyleGuideView.swift
@@ -10,17 +10,32 @@ import SwiftUI
 struct StyleGuideView: View {
     var body: some View {
         VStack {
-            
-            ColorSampleView(colorName: "AccentColor")
-            ColorSampleView(colorName: "DividerColor")
-            ColorSampleView(colorName: "PrimaryColor")
-            ColorSampleView(colorName: "PrimaryDarkColor")
-            ColorSampleView(colorName: "PrimaryLightColor")
-            ColorSampleView(colorName: "TextColor")
-            ColorSampleView(colorName: "TextPrimaryColor")
-            ColorSampleView(colorName: "TextSecondaryColor")
+            VStack {
+                Text("Color samples")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("TextPrimaryColor"))
+                
+                ColorSampleView(colorName: "AccentColor")
+                ColorSampleView(colorName: "DividerColor")
+                ColorSampleView(colorName: "PrimaryColor")
+                ColorSampleView(colorName: "PrimaryDarkColor")
+                ColorSampleView(colorName: "PrimaryLightColor")
+                ColorSampleView(colorName: "TextColor")
+                ColorSampleView(colorName: "TextPrimaryColor")
+                ColorSampleView(colorName: "TextSecondaryColor")
 
+            }
+            VStack {
+                Text("Gradients")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("TextPrimaryColor"))
+                
+                
+            }
         }
+        
         .padding()
         .background(Color("BackgroundColor"))
     }
@@ -30,8 +45,13 @@ struct StyleGuideView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             StyleGuideView()
+                .frame(width: 400.0)
+                .previewLayout(.sizeThatFits)
+                
             StyleGuideView()
                 .environment(\.colorScheme, .dark)
+                .frame(width: 400.0)
+                .previewLayout(.sizeThatFits)
         }
     }
 }
@@ -46,8 +66,8 @@ struct ColorSampleView: View {
             Spacer()
             Color(colorName)
                 .frame(minWidth: 100,
-                       idealWidth: 150,
-                       maxWidth: 160,
+                       idealWidth: 130,
+                       maxWidth: 140,
                        minHeight: 40,
                        idealHeight: 80,
                        maxHeight: 90)
