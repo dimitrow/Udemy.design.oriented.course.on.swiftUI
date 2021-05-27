e7f90424cb1a411e4d7f65b2bfd643501fabb371
gene <e.dimitrow@gmail.com>
2021-05-27 11:06:27 +0300

    -initial

---
+++ Udemy.DOC.EmojiApp/Udemy_DOC_EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Udemy_DOC_EmojiAppApp.swift
+//  Udemy.DOC.EmojiApp
+//
+//  Created by Gene Dimitrow on 27.05.21.
+//
+
+import SwiftUI
+
+@main
+struct Udemy_DOC_EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
