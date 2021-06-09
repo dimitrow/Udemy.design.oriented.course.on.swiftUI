d68a542c8c86d3f2cbc566493fa9d57eb0136736
gene <e.dimitrow@gmail.com>
2021-06-09 19:10:51 +0300

    -initial

---
+++ ProbeAlles/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProbeAlles
+//
+//  Created by Gene Dimitrow on 09.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProbeAlles/ProbeAllesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProbeAllesApp.swift
+//  ProbeAlles
+//
+//  Created by Gene Dimitrow on 09.06.21.
+//
+
+import SwiftUI
+
+@main
+struct ProbeAllesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
