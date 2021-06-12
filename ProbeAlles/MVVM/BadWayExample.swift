a192d2236c1e91db0e8de172a97a9becdd3070ad
gene.dimitrow <e.dimitrow@gmail.com>
2021-06-12 13:41:51 +0300

    -done: MVVM bad example // 120

---
+++ ProbeAlles/MVVM/BadWayExample.swift
@@ -0,0 +1,67 @@
+//
+//  BadWayExample.swift
+//  ProbeAlles
+//
+//  Created by Gene Dimitrow on 12.06.21.
+//
+
+import SwiftUI
+
+struct Product: Identifiable {
+    
+    let id: UUID = UUID()
+    let name: String
+}
+
+
+struct BadWayExample: View {
+    
+    @State var products = [Product(name: "first"), Product(name: "second"), Product(name: "third"), Product(name: "fourth"), Product(name: "fifth"), Product(name: "sixth"),]
+    
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            List {
+                ForEach(products) { product in
+                    
+                    NavigationLink(destination: Text("DESTINATION: \n\(product.name)")) {
+                        
+                        Text(product.name)
+                    }
+                    
+                }.onDelete { indexSet in
+                    for index in indexSet {
+                        products.remove(at: index)
+                    }
+                }
+                .onMove { indexSet, newOffset in
+                    
+                    products.move(fromOffsets: indexSet, toOffset: newOffset)
+                }
+                
+            }
+            .navigationBarTitle(Text("Products"), displayMode: .large)
+            .toolbar {
+                ToolbarItemGroup(placement: ToolbarItemPlacement.navigationBarTrailing) {
+                    
+                    EditButton()
+                    Button {
+                        products.append(Product(name: "newly added product"))
+                        
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+struct BadWayExample_Previews: PreviewProvider {
+    static var previews: some View {
+        BadWayExample()
+    }
+}
